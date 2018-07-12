ec97c7d64000d0a20a6695fbed11f16bed797eb2
stephenhky <stephenhky@yahoo.com.hk>
2018-07-12 11:27:27 -0400

    removed bugs due to smoothing before taking logarithms on counts

--- ConversionScript.R
+++ ConversionScript.R
@@ -28,16 +28,19 @@ cell_types<- unlist(zeisel.oridata[8,][3:(dim(zeisel.oridata)[2])])
 # creating single-cell experiment object
 zeisel.sce<- SingleCellExperiment(
   assays = list(counts=zeisel.fulldata,
-                logcounts=log2(zeisel.fulldata)+1
+                logcounts=log2(zeisel.fulldata+1)
                 )
 )
 rownames(zeisel.sce)<- gene_names
 colnames(zeisel.sce)<- unname(unlist(zeisel.oridata[7, 3:3007]))
-zeisel.sce$cell_type<- cell_types
+zeisel.sce$cell_type<- unname(cell_types)
 
 # further wrangling
 rowData(zeisel.sce)$feature_symbol<- rownames(zeisel.sce)
 zeisel.sce<- zeisel.sce[!duplicated(rowData(zeisel.sce)$feature_symbol), ]
+
+# define spike-ins
+isSpike(zeisel.sce, "ERCC") <- grepl("ERCC", rowData(zeisel.sce)$feature_symbol)
 
 file.remove('./expression_mRNA_17-Aug-2014.txt')
 saveRDS(zeisel.sce, 'Target/zeisel_dataset_sce.rds')
